087f634e2ae08c5d92e240f7e2611a01921492e0
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-12-01 20:28:18 -0300

    retirando o suposto "trojan"

--- FilaupeChallenge01/ChallengeWSSoftware09/ChallengeWSSoftware09.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ChallengeWSSoftware09.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
