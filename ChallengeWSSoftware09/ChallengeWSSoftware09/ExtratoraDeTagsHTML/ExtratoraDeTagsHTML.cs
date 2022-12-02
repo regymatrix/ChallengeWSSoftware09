ff23f16ab3c0c796802b3d6ac6d1fa2b38a37504
PedroAssencao <115711899+PedroAssencao@users.noreply.github.com>
2022-12-01 21:08:17 -0300

    Pedro

---
+++ ChallengeWSSoftware09/ChallengeWSSoftware09/ExtratoraDeTagsHTML/ExtratoraDeTagsHTML.cs
@@ -0,0 +1,47 @@
+﻿namespace ChallengeWSSoftware09.ExtratoraDeTagsHTML
+{
+    public class ExtratoraDeTagsHTML
+    {
+        public int qunatidadeDeLetras(string texto) 
+        {
+
+            int x = texto.Count();
+            return x;
+        
+        }
+
+        public int quantidadeDeVogais(string texto) 
+        {
+
+            return texto.Count(x => (x == 'a') || (x == 'e') || (x == 'i') || (x == 'o') || (x == 'u'));
+
+
+        }
+
+        public string ExtrairTagIMG(string texto) 
+        {
+
+            return texto.Substring(6, 27);
+            return texto.Substring(11, 34);
+        
+        }
+
+        public string ExtrairEnderecoAtributoSRC(string texto) 
+        {
+
+            return texto.Substring(16, 29);
+            return texto.Substring(19, 43);       
+        
+        }
+
+        public string ExtrairTagH1(string texto) 
+        {
+
+            return texto.Substring(39, 58);
+            return texto.Substring(8, 17);
+        
+     
+        
+        }
+    }
+}
