6133d3582d25e2b51bd9d49c0bacff2882ce0a18
Android <110397800+Celsooz@users.noreply.github.com>
2022-12-01 21:16:37 -0300

    Vinicius

---
+++ Desafio/Desafio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
