6db4d296649a3dd66639ce8cb01543e42db36130
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-12-02 05:24:08 -0300

    Corrigido pq não custa nada

--- FilaupeChallenge01/ChallengeWSSoftware09/ChallengeWSSoftware09.Library/ExtratoraDeTagsHTML.cs
+++ FilaupeChallenge01/ChallengeWSSoftware09/ChallengeWSSoftware09.Library/ExtratoraDeTagsHTML.cs
@@ -10,7 +10,7 @@ namespace ChallengeWSSoftware09.Library
 
         public string ExtrairTagIMG(string texto) => Regex.Split(texto, @"(<img .+?>|/>)").First(t => t.Contains("<img"));
 
-        public string ExtrairEnderecoAtributoSRC(string texto) => Regex.Split(texto, @"src=(.*.jpg|.png|.jpeg|.gif|.bmp|.psd|.tiff|.pdf|.svg)").First(c => c.Contains('"')).Replace("\"", "");
+        public string ExtrairEnderecoAtributoSRC(string texto) => Regex.Split(texto, @"src=(.*\.jpg|\.png|\.jpeg|\.gif|\.bmp|\.psd|\.tiff|\.pdf|\.svg)").First(c => c.Contains('"')).Replace("\"", "");
 
         public string ExtrairTagH1(string texto) => Regex.Split(texto, @"(<h1>.*</h1>)").First(t => t.Contains("h1"));
     }
