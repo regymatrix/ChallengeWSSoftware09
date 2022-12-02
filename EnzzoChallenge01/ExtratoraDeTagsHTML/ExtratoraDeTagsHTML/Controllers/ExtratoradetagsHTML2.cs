7c6187486555cfd5a1b8888d65f67ba6bad717fc
enzzoh <63256458+enzzoh@users.noreply.github.com>
2022-12-01 21:20:45 -0300

    EnzzoChallenge01

---
+++ EnzzoChallenge01/ExtratoraDeTagsHTML/ExtratoraDeTagsHTML/Controllers/ExtratoradetagsHTML2.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace ExtratoraDeTagsHTML.Controllers
+{
+    public class ExtratoradetagsHTML2
+    {
+        public int qtdDeLetras(string texto)
+        {
+
+            int x = texto.Count();
+            return x;
+
+
+        }
+        public int qtdvogais(string texto)
+        {
+
+            return texto.Count(x => (x == 'a') || (x == 'e') || (x == 'i') || (x == 'o') || (x == 'u'));
+
+
+        }
+  
+        
+        
+        
+
+    }
+}
