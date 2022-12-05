d3498afc94fc5d87ddc0800d0643c0791457d9bd
DEVJS2005 <jvl14533@outlook.com>
2022-12-05 20:33:10 -0300

    Desafio 01

---
+++ Desafio 01 João Victor/ExtratoraDeTagsHTML.cs
@@ -0,0 +1,54 @@
+﻿namespace Desafio_01_João_Victor
+{
+    public class ExtratoraDeTagsHTML
+    {
+        public int quantidadeDeLetras(string texto)
+        {
+
+            int totalLetra = texto.Length - 4;
+
+            return totalLetra;
+        }
+
+        public int quantidadeDeVogais(string texto)
+        {
+            char[] vogais = new char[10] { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            char[] palavra = texto.ToCharArray();
+            int contadorVogais = 0;
+
+            for (int i = 0; palavra.Length > vogais.Length; i++)
+            {
+                for (int f = 0; palavra.Length > vogais.Length; f++)
+                {
+                    if (palavra[f] == vogais[i])
+                    {
+                        contadorVogais++;
+                    }
+                }
+
+            }
+            return contadorVogais;
+        }
+
+        public string ExtrairTagIMG(string texto)
+        {
+            string tag = texto.Substring(texto.IndexOf("<"), texto.LastIndexOf("/>"));
+
+            return tag;
+        }
+
+        public string ExtrairEnderecoAtributoSRC(string texto)
+        {
+            string valueSrc = texto.Substring(texto.IndexOf($"="), texto.LastIndexOf(" "));
+
+            return valueSrc;
+        }
+
+        public string ExtrairTagH1(string texto)
+        {
+            string valueH1 = texto.Substring(texto.IndexOf($"<h1>"), texto.LastIndexOf("</h1>"));
+
+            return valueH1;
+        }
+    }
+}
